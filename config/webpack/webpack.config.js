24430cc673c0cec19060fbdfd449ef4f8288e6cb
Ivan Martinez Laborda <ivan@MacBook-MacBook-Pro-de-Ivan.local>
2019-03-23 17:36:18 +0100

    webpack configuration, babel, linters

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackMerge = require('webpack-merge')
+
+const modeConfig = env => require(`./webpack.${env}.js`)(env)
+
+module.exports = ({mode, presets} = {mode: 'production', presets: []}) => {
+  console.log('mode', mode)
+  return webpackMerge(
+    {
+      mode,
+      module: {
+        rules: [
+          {
+            test: /(\.js)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          }
+        ]
+      },
+      stats: {
+        entrypoints: false,
+        children: false
+      },
+      plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new webpack.ProgressPlugin()
+      ]
+    },
+    modeConfig(mode)
+  )
+}
