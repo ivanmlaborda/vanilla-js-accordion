edfc48aceda5db0a297ea430b3f2d1c7dbfc9604
Ivan Martinez Laborda <ivan@macbookbpdeivan.home>
2019-03-25 22:29:29 +0100

    Feed accordion with ajax data

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -7,6 +7,7 @@ export default class Accordion {
     this._mainNode = null
     this._childs = []
     this._markup = template
+    this._insertElement = this._insertElement.bind(this)
 
     this._init()
   }
@@ -48,7 +49,26 @@ export default class Accordion {
       : description.scrollHeight + 'px'
   }
 
+  _insertElement(term) {
+    const element = `<dt>${term.term}</dt>`
+    const description = `<dd><p>${term.description}</p></dd>`
+
+    this._mainNode.querySelector('dl').insertAdjacentHTML('beforeend', element)
+    this._mainNode
+      .querySelector('dl')
+      .lastChild.addEventListener('click', this._handleClick)
+
+    this._mainNode
+      .querySelector('dl')
+      .insertAdjacentHTML('beforeend', description)
+
+    this._childs = [
+      ...this._childs,
+      this._mainNode.querySelector('dl').lastChild
+    ]
+  }
+
   addTo(termsArr) {
-    console.log('terms arr', termsArr)
+    termsArr.forEach(this._insertElement)
   }
 }
