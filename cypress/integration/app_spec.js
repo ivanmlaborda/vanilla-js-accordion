6d01e96e90e364ff4dde7cece16935d746eeecde
Ivan Martinez Laborda <i.martinez.laborda@accenture.com>
2019-03-29 15:21:59 +0100

    Add cypress tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,37 +1,123 @@
-describe('Acceptation', () => {
+describe('Acceptation sync data accordion', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
   it('#root container has a Accordion', () => {
     cy.get('#root')
-      .get('dl')
+      .find('dl')
       .should('have.class', 'Accordion')
   })
 
   it('Accordion has a dt element with class "Accordion-term"', () => {
     cy.get('#root')
-      .get('.Accordion')
-      .get('dt')
+      .find('.Accordion')
+      .find('dt')
       .should('have.class', 'Accordion-term')
   })
 
   it('Accordion has a dd element with class "Accordion-description"', () => {
     cy.get('#root')
-      .get('.Accordion')
-      .get('dd')
+      .find('.Accordion')
+      .find('dd')
       .should('have.class', 'Accordion-description')
   })
 
-  it('Clicks on dt elements toggle its class to "is-active"', () => {
+  it('Clicks on dt elements toggle class "is-active"', () => {
     cy.get('#root')
-      .get('.Accordion')
-      .get('dt')
-      .click({multiple: true})
+      .find('.Accordion')
+      .find('dt')
+      .first()
+      .click()
+      .should('have.class', 'is-active')
 
     cy.get('#root')
-      .get('.Accordion')
-      .get('dt')
+      .find('.Accordion')
+      .find('dt')
+      .first()
+      .click()
+      .should('not.have.class', 'is-active')
+  })
+
+  it('Clicks on dt elements extends a description element', () => {
+    cy.get('#root')
+      .find('.Accordion')
+      .find('dt')
+      .last()
+      .click()
+
+    cy.get('#root')
+      .find('.Accordion')
+      .find('dd')
+      .last()
+      .should('have.class', 'is-extended')
+  })
+})
+
+describe('Acceptation AJAX data accordion', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route('/api/terms').as('terms')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('#root container has a Accordion', () => {
+    cy.get('#root2')
+      .find('dl')
+      .should('have.class', 'Accordion')
+  })
+
+  it('Accordion has a dt element with class "Accordion-term"', () => {
+    cy.get('#root')
+      .find('.Accordion')
+      .find('dt')
+      .should('have.class', 'Accordion-term')
+  })
+
+  it('Accordion has a dd element with class "Accordion-description"', () => {
+    cy.get('#root')
+      .find('.Accordion')
+      .find('dd')
+      .should('have.class', 'Accordion-description')
+  })
+
+  it('Clicks on dt elements toggle class "is-active"', () => {
+    cy.get('#root')
+      .find('.Accordion')
+      .find('dt')
+      .first()
+      .click()
       .should('have.class', 'is-active')
+
+    cy.get('#root')
+      .find('.Accordion')
+      .find('dt')
+      .first()
+      .click()
+      .should('not.have.class', 'is-active')
+  })
+
+  it('Clicks on dt elements extends a description element', () => {
+    cy.wait('@terms')
+    cy.get('#root2')
+      .find('.Accordion')
+      .find('dt')
+      .last()
+      .click()
+
+    cy.get('#root2')
+      .find('.Accordion')
+      .find('dd')
+      .last()
+      .should('have.class', 'is-extended')
+  })
+
+  it('Should have 5 dt elements', () => {
+    cy.wait('@terms')
+    cy.get('#root2')
+      .find('dt')
+      .should($dt => {
+        expect($dt).to.have.length(5)
+      })
   })
 })
