c39b0f4cb6a21fd77ff18018dc5ec57f61222948
Ivan Martinez Laborda <ivan@192.168.1.131>
2019-03-31 00:52:59 +0100

    Refactor Terms service test

--- src/tests/services/Terms.test.js
+++ src/tests/services/Terms.test.js
@@ -1,26 +1,14 @@
-import Terms from '../../services/Terms'
+import terms from '../../services/Terms'
+import termsFixture from '../fixtures/terms.json'
 import axios from 'axios'
 
 jest.mock('axios')
 
 describe('Terms service', () => {
   it('should fetch terms', () => {
-    const terms = {
-      data: [
-        {
-          term: 'Section 4 - AJAX',
-          description: 'Section 4 Content...'
-        },
-        {
-          term: 'Section 5 - AJAX',
-          description: 'Section 5 Content...'
-        }
-      ]
-    }
-
-    const resp = {data: {data: terms}}
+    const resp = {data: {data: termsFixture}}
     axios.get.mockResolvedValue(resp)
 
-    return Terms.get().then(res => expect(res).toEqual(terms))
+    return terms.get().then(res => expect(res).toEqual(termsFixture))
   })
 })
